f31bbee0467e475325a916552908913c0455ceaf
LucidJoy <jyotirmoykarmakar02@gmail.com>
2022-06-06 21:12:06 +0530

    Top Bids & NFT Create page design

--- README.md
+++
@@ -1,1 +0,0 @@
-# NFT MarketPlace Platform -> LucidJoy
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import { useState, useEffect, useRef } from "react";
 import Image from "next/image";
 import { useTheme } from "next-themes";
 
-import { Banner, CreatorCard } from "../components";
+import { Banner, CreatorCard, NFTCard } from "../components";
 import { makeId } from "../utils/makeId";
 import images from "../assets";
 
@@ -103,6 +103,30 @@ const Home = () => {
                 </>
               )}
             </div>
+          </div>
+        </div>
+
+        <div className='mt-10'>
+          <div className='flexBetween mx-4 xs:mx-0 minlg:mx-8 sm:flex-col sm:items-start'>
+            <h1 className='font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold sm:mb-4 flex-1'>
+              Hot Bids
+            </h1>
+            <div>Searchbar</div>
+          </div>
+          <div className='mt-3 w-full flex flex-wrap justify-start md:justify-center'>
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) => (
+              <NFTCard
+                key={`nft-${i}`}
+                nft={{
+                  i: i,
+                  name: `Nifty NFT ${i}`,
+                  price: 10 - i * 0.5,
+                  seller: `0x${makeId(3)}...${makeId(4)}`,
+                  owner: `0x${makeId(3)}...${makeId(4)}`,
+                  description: "Cool NFT on Sale",
+                }}
+              />
+            ))}
           </div>
         </div>
       </div>
